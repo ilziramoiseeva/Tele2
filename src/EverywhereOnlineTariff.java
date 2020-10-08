905aac083d6b891d47fed7208d0047affc8f9e5c
ilziramoiseeva <ilzirkam@gmail.com>
2020-10-08 10:55:04 +0300

    Min.java add

---
+++ src/EverywhereOnlineTariff.java
@@ -0,0 +1,12 @@
+public class EverywhereOnlineTariff {
+    String id;
+    String tariffName;
+    int minutes;
+    boolean unlimitedMinutes;
+    int internetTraffic;
+    boolean unlimitedTraffic;
+    String imageUrl;
+    boolean bestseller;
+    int price;
+    String buySimUrl;
+}
---
+++ src/MyOnlineTariff.java
@@ -0,0 +1,13 @@
+public class MyOnlineTariff {
+    String id;
+    String tariffName;
+    int minutes;
+    boolean unlimitedMinutes;
+    int internetTraffic;
+    boolean unlimitedTraffic;
+    String imageUrl;
+    int price;
+    String buySimUrl;
+    String tariffSetting;
+
+}
